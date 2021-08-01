cc48ae416aef87e593e6df898c58c4158f6a9231
Bibekbvk <bhattaraibvk@gmail.com>
2021-08-01 20:47:39 +0545

    dsfdsf

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use('/api/',apiRouter);
 
 
 app.listen(process.env.PORT||'3000',()=>{
+  
 
-    console.log(`Server is running on port: ${process.env.PORT || "3000"}`)
+    console.log(`Server is runnning on port: ${process.env.PORT || "3000"}`)
 });
