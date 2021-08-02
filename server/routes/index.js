db3c2bc2439ccd45d32dfb21232a325f6acee5af
Bibekbvk <bhattaraibvk@gmail.com>
2021-08-02 14:18:28 +0545

    rfdsfsd

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const authorize = require('../middleware/auth-middleware.js');
 const router = express.Router();
 
 router.get('/allcategories', async(req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin:*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     try{
