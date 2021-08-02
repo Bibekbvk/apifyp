1b3ae952eade5fc6e57d12860d5f597fa021ea16
Bibekbvk <bhattaraibvk@gmail.com>
2021-08-02 14:33:37 +0545

    sdfsf

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const authorize = require('../middleware/auth-middleware.js');
 const router = express.Router();
 
 router.get('/allcategories', async(req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin:*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     try{
@@ -58,7 +58,7 @@ router.get('/search', async(req,res,next) => {
 
 
 router.get('/getwallpaper', async (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin:*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     try {
