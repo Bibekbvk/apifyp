221df25619f017ba4cb232e8a56988454ee6eb61
Bibekbvk <bhattaraibvk@gmail.com>
2021-08-02 21:00:00 +0545

    dsfgsdg

--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const cors = require('cors')
-//CORS policy
+
+const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
 res.header("Access-Control-Allow-Origin", "*");
 res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 next();
 });
+
 const apiRouter =require('./routes');
-const app = express();
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const cors = require('cors')
+//CORS policy
 
 // to use env values
 require('dotenv').config()
